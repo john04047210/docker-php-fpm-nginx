e4a9e58ba8a26ad09a5d3ba34f25bc1ebff66c93
john04047210 <john_04047210@163.com>
2018-06-20 18:36:40 +0900

    Modify create-instance.sh
    composer命令执行失败处理

--- scripts/create-instance.sh
+++ scripts/create-instance.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
+# 下面命令执行失败时执行这个命令检查失败原因
+# composer diag
+# --------------------------------------
+# Checking pubkeys: FAIL
+# Missing pubkey for tags verification
+# Missing pubkey for dev verification
+# Run composer self-update --update-keys to set them up
+# --------------------------------------
+# composer self-update --update-keys
+# 提示输入Dev/Snapshot 信息，浏览器访问如下地址获取Key信息
+# https://composer.github.io/pubkeys.html
+#
+
 composer global require --no-plugins --no-scripts "laravel/installer"
